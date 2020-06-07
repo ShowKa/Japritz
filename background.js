081a1ba2f023d79d82fc2323fd37916c4ee1fc88
John Doe <johndoe@example.com>
2020-06-07 09:04:37 +0900

    tmp....

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
+// browser button
+chrome.browserAction.onClicked.addListener(function (tab) {
+	callJapritz(tab);
+});
+// 右クリック contextmenus
+chrome.contextMenus.create({
+	"title": "Japritz",
+	"contexts": ["selection"],
+	onclick: function (info, tab) {
+		callJapritz(tab);
+	}
+});
+// load tokenizer
+var dictUrl = chrome.extension.getURL("lib/dict/");
+var tokenizer;
+kuromoji.builder({ dicPath: dictUrl }).build(function (err, _tokenizer) {
+	tokenizer = _tokenizer;
+});
 // 起動
 function callJapritz(tab) {
 	// get option
@@ -10,17 +28,3 @@ function callJapritz(tab) {
 		}, null);
 	});
 }
-
-// browser button
-chrome.browserAction.onClicked.addListener(function (tab) {
-	callJapritz(tab);
-});
-
-// 右クリック contextmenus
-chrome.contextMenus.create({
-	"title": "Japritz",
-	"contexts": ["selection"],
-	onclick: function (info, tab) {
-		callJapritz(tab);
-	}
-});
